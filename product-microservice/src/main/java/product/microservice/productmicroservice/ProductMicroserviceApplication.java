02a56862daf2eb658a0d6780c6f8206bedbd09c2
merimakovacevic <mkovacevic1@etf.unsa.ba>
2021-03-31 11:20:34 +0200

    Eureka server still not working

--- product-microservice/src/main/java/product/microservice/productmicroservice/ProductMicroserviceApplication.java
+++ product-microservice/src/main/java/product/microservice/productmicroservice/ProductMicroserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Component;
 import product.microservice.productmicroservice.model.Product;
 import product.microservice.productmicroservice.model.ProductType;
@@ -15,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
